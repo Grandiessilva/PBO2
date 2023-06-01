c5c4be8d987c3719e84a993a0738d27e36eb8cb9
Grandiessilva <127475726+Grandiessilva@users.noreply.github.com>
2023-06-01 13:48:35 +0700

    Add files via upload

---
+++ Praktikum3/overload1.py
@@ -0,0 +1,6 @@
+# Nama  : Grandies Silvania Verdianto
+# Kelas : R1
+# NIM   : 210511003
+
+print(len("geeks"))
+print(len([10, 20, 30]))
---
+++ Praktikum3/overload2.py
@@ -0,0 +1,7 @@
+# Nama  : Grandies Silvania Verdianto
+# Kelas : R1
+# NIM   : 210511003
+
+print(5 - 1)
+print("Above" + " The" + " Clouds")
+print([1, 2] + [3, 4]) 
